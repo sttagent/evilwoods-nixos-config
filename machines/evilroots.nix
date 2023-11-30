7e10253bef92693ad5f3ee8a3f16aad52968ffff
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2023-11-30 15:42:52 +0100

    fix formating

--- machines/evilroots.nix
+++ machines/evilroots.nix
@@ -1,66 +1,67 @@
 ({lib, pkgs, ...}: {
-            networking.hostName = "evilroots";
+  networking.hostName = "evilroots";
 
-            nix = {
-	            package = pkgs.nixFlakes;
-	            extraOptions = ''
-              	experimental-features = nix-command flakes
-              '';
-	          };
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+	};
 
-            nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-            evilcfg.ssh = true;
-            evilcfg.desktop = true;
-            evilcfg.nvidia = true;
-            #evilcfg.steam = true;
-            evilcfg.zsa = true;
-            evilcfg.podman = true;
+  evilcfg.ssh = true;
+  evilcfg.desktop = true;
+  evilcfg.nvidia = true;
+  #evilcfg.steam = true;
+  evilcfg.zsa = true;
+  evilcfg.podman = true;
 
-	    # LTU VPN
-	    services.strongswan.enable = true;
-	    networking.networkmanager.enableStrongSwan = true;
+	# LTU VPN
+	services.strongswan.enable = true;
+	networking.networkmanager.enableStrongSwan = true;
 
-            # docker setup
-            virtualisation.docker = {
-              enable = true;
-              rootless = {
-                enable = true;
-                setSocketVariable = true;
-              };
-              storageDriver = "btrfs";
-            };
+  # docker setup
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
 
-            boot.initrd = {
-              availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-              verbose = false;
-            };
-            boot.kernelModules = [ "kvm-intel" ];
+    storageDriver = "btrfs";
+  };
 
-            networking.useDHCP = lib.mkDefault true;
-            # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+  boot.initrd = {
+    availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+    verbose = false;
+  };
+  boot.kernelModules = [ "kvm-intel" ];
 
-            hardware.cpu.intel.updateMicrocode = lib.mkDefault true;
-            hardware.enableRedistributableFirmware = lib.mkDefault true;
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault true;
+  hardware.enableRedistributableFirmware = lib.mkDefault true;
             # Use the systemd-boot EFI boot loader.
-            boot = {
-              loader = {
-                efi = {
-                  canTouchEfiVariables = true;
-                };
-                systemd-boot.enable = true;
-              };
-              kernelPackages = pkgs.linuxPackages_latest;
-              #kernelPackages = pkgs.linuxPackages_6_1; # nvidia incopatible with linuxPackages_latest
-              #consoleLogLevel = 0;
-              plymouth.enable = true;
-              kernelParams = [
-                "quiet"
-                "splash"
-                #"rd.systemd.show_status=false"
-              ];
-            };
+  boot = {
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+      };
+      systemd-boot.enable = true;
+    };
+    kernelPackages = pkgs.linuxPackages_latest;
+    #kernelPackages = pkgs.linuxPackages_6_1; # nvidia incopatible with linuxPackages_latest
+    #consoleLogLevel = 0;
+    plymouth.enable = true;
+    kernelParams = [
+      "quiet"
+      "splash"
+      #"rd.systemd.show_status=false"
+    ];
+  };
 
             # Version of NixOS installed from live disk. Needed for backwards compatability.
-            system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05"; # Did you read the comment?
 })
