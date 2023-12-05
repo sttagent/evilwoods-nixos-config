49870faf2c73410d7fa9037c475a3a891664ffbf
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2023-12-05 21:23:53 +0100

    add fn that filter hosts in hosts folder

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,7 @@
-rec {
+{
+  lib,
+  ...
+}: rec {
   defaultSystem = "x86_64-linux";
 
   mkPkgs = { channel, system ? defaultSystem, cfg ? { allowUnfree = true; } }:
@@ -6,4 +9,18 @@ rec {
       inherit system;
       config = cfg;
     };
+
+  mkHosts = hostsDir: 
+    lib.filterAttrs (name: value:
+      value != null)
+    (lib.mapAttrs' (name: value:
+      let path = "${hostsDir}/${name}"; in
+      if value == "directory" && builtins.pathExists "${path}/default.nix"
+      then lib.nameValuePair name value
+      else if value == "regular" && 
+              name != "default.nix" &&
+              lib.hasSuffix ".nix" name
+      then lib.nameValuePair (lib.removeSuffix ".nix" name) value
+      else lib.nameValuePair "" null)
+    (builtins.readDir hostsDir));
 }
