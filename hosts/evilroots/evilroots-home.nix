3d8c866be81531db4e63230fd257de44b244fb3c
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2023-12-14 15:18:50 +0100

    configure lsp zero

--- hosts/evilroots/evilroots-home.nix
+++ hosts/evilroots/evilroots-home.nix
@@ -333,7 +333,25 @@ in
             nvim-treesitter-context
 
             nvim-lspconfig
-            lsp-zero-nvim
+            {
+              plugin = lsp-zero-nvim;
+              type = "lua";
+              config = ''
+                local lsp = require('lsp-zero').preset()
+
+                lsp.on_attach(function(client, bufnr)
+                  lsp.default_keymaps({buffer = bufnr})
+                end)
+
+                require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+                require('lspconfig').nixd.setup(lsp.nvim_lua_ls())
+
+                require('lspconfig').ansiblels.setup(lsp.nvim_lua_ls())
+
+                require('lspconfig').pylyzer.setup(lsp.nvim_lua_ls())
+              '';
+            }
 
             {
               plugin = copilot-lua;
