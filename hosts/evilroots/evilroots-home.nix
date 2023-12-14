20c20d60d36d8d1f4f50d3932778c18721ccdf77
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2023-12-14 12:47:43 +0100

    enable treesitter

--- hosts/evilroots/evilroots-home.nix
+++ hosts/evilroots/evilroots-home.nix
@@ -304,6 +304,32 @@ in
             nvim-lspconfig
             lsp-zero-nvim
             nvim-treesitter.withAllGrammars
+            {
+              plugin = nvim-treesitter.withAllGrammars;
+              type = "lua";
+              config = ''
+                vim.opt.foldmethod = 'expr'
+                vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+                vim.opt.foldenable = false
+
+                require('nvim-treesitter.configs').setup({
+                  highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                  },
+
+                  incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                      init_selection = "gnn", -- set to `false` to disable one of the mappings
+                      node_incremental = "grn",
+                      scope_incremental = "grc",
+                      node_decremental = "grm",
+                    },
+                  },
+                })
+              '';
+            }
             nvim-treesitter-textobjects
             nvim-treesitter-context
             gruvbox-nvim
