50a82c67dcaa04720e77be1623c3279d66fae05e
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2023-12-14 12:48:33 +0100

    enable nvim cmp

--- hosts/evilroots/evilroots-home.nix
+++ hosts/evilroots/evilroots-home.nix
@@ -361,12 +361,47 @@ in
             neorg-telescope
 
             copilot-cmp
-            nvim-cmp
             cmp_luasnip
             cmp-nvim-lsp
             cmp-buffer
             cmp-path
             cmp-nvim-lua
+            {
+              plugin = nvim-cmp;
+              type = "lua";
+              config = ''
+                local cmp = require('cmp')
+                cmp.setup({
+                  snippet = {
+                    expand = function(args)
+                      require('luasnip').lsp_expand(args.body)
+                    end,
+                  },
+
+                  window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                  },
+
+                  mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                  }),
+
+                  sources = cmp.config.sources({
+                    { name = "luasnip"},
+                    { name = "buffer"},
+                    { name = "path"},
+                    { name = "nvim_lua"},
+                    { name = "nvim_lsp"},
+                    { name = "copilot_cmp"},
+                  }),
+                })
+              '';
+            }
 
             luasnip
             {
