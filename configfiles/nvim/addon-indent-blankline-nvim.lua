7d7bd7c8ceab270fba381278a1c1258d127165c7
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2023-12-15 11:50:01 +0100

    move nvim indent-blakline config to lua file

---
+++ configfiles/nvim/addon-indent-blankline-nvim.lua
@@ -0,0 +1,10 @@
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
+require('ibl').setup({
+    exclude = {
+        filetypes = {
+            'dashboard',
+        },
+    },
+})
--- hosts/evilroots/evilroots-home-nvim.nix
+++ hosts/evilroots/evilroots-home-nvim.nix
@@ -91,17 +91,8 @@ in with builtins; {
       {
         plugin = indent-blankline-nvim;
         type = "lua";
-        config = /* lua */ ''
-            vim.opt.list = true
-            vim.opt.listchars:append "space:⋅"
-            vim.opt.listchars:append "eol:↴"
-            require('ibl').setup({
-                exclude = {
-                    filetypes = {
-                        'dashboard',
-                    },
-		},
-            })
+        config = ''
+          ${readFile /${nvimConfigDir}/addon-indent-blankline-nvim.lua}
         '';
       }
 
