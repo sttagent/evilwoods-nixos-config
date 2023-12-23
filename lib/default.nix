00f89de2d388a3db83b45d3a480d4355e04e9c37
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2023-12-23 22:39:05 +0100

    add with builtins

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,6 @@
 { lib
 , ...
-}: rec {
+}: with builtins; rec {
   defaultSystem = "x86_64-linux";
 
   # filter out null attributes
@@ -10,15 +10,16 @@
   fitlerMapHosts = hostDir: attrs:
     filterNullHosts (lib.mapAttrs'
       (name: value:
-        let path = "${builtins.toString hostDir}/${name}"; in
-        if value == "directory" && builtins.pathExists "${path}/default.nix"
+        let
+          path = "${toString hostDir}/${name}";
+        in if value == "directory" && pathExists "${path}/default.nix"
         then lib.nameValuePair name (mkHost path attrs)
         else if value == "regular" &&
           name != "default.nix" &&
           lib.hasSuffix ".nix" name
         then lib.nameValuePair (lib.removeSuffix ".nix" name) (mkHost path attrs)
         else lib.nameValuePair "" null)
-      (builtins.readDir hostDir));
+      (readDir hostDir));
 
   # make nixos system from a single nix file or directory
   mkHost = hostPath: { system ? defaultSystem, inputs, extraModules, ... }: lib.nixosSystem {
