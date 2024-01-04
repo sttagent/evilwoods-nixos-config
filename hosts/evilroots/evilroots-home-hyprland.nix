80bac0b3192ab7d7d595efb460297921fe8fb5ab
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2024-01-04 19:59:37 +0100

    configure monitors

--- hosts/evilroots/evilroots-home-hyprland.nix
+++ hosts/evilroots/evilroots-home-hyprland.nix
@@ -6,6 +6,13 @@ in
 with builtins; {
   home-manager.users.${mainUser}.wayland.windowManager.hyprland = {
     enable = true;
+    settings = {
+      monitor = [
+        "DP-3, 2560x1440@144, 0x0, 1"
+        "HDMI-A-1 ,1920x10080@60, 2560x360, 1"
+      ];
+
+    };
     extraConfig = ''
       ${readFile ./${hyprConfigDir}/hyprland.conf}
     '';
