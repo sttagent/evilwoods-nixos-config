798d1ba7dfeb81d298899e8a99e3ee6d07c2710e
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2024-01-05 11:46:10 +0100

    refractor hyprland module

--- hosts/evilroots/evilroots-home-hyprland.nix
+++ hosts/evilroots/evilroots-home-hyprland.nix
@@ -4,18 +4,36 @@ let
   hyprConfigDir = "../../configfiles/hypr";
 in
 with builtins; {
-  home-manager.users.${mainUser}.wayland.windowManager.hyprland = {
-    enable = true;
-    settings = {
-      monitor = [
-        "DP-3, 2560x1440@144, 0x0, 1"
-        "HDMI-A-1 ,1920x10080@60, 2560x360, 1"
-      ];
+  home-manager.users.${mainUser} =  {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      settings = {
+        env =   [
+          "LIBVA_DRIVER_NAME,nvidia"
+          "XDG_SESSION_TYPE,wayland"
+          "GBM_BACKEND,nvidia-drm"
+          "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+          "WLR_NO_HARDWARE_CURSORS,1"
+        ];
 
-      exec-once = "waybar &";
+        monitor = [
+          "DP-3, 2560x1440@144, 0x0, 1"
+          "HDMI-A-1 ,1920x10080@60, 2560x360, 1"
+        ];
+
+        exec-once = [
+          "waybar &"
+        ];
+      };
+      extraConfig = ''
+        ${readFile ./${hyprConfigDir}/hyprland.conf}
+      '';
     };
-    extraConfig = ''
-      ${readFile ./${hyprConfigDir}/hyprland.conf}
-    '';
+
+    services.mako.enable = true;
+    programs = {
+      waybar.enable = true;
+      wofi.enable = true;
+    };
   };
 }
--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -8,30 +8,81 @@ in
     enableHyprland = mkEnableOption "Hyprland window manager";
   };
 
-  config = mkIf cfg.enableHyprland {
-    programs.hyprland = {
-      enable = true;
-      xwayland.enable = true;
-    };
-
-    xdg.portal = {
-      enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal
-        # xdg-desktop-portal-gtk
-      ];
-    };
-
-    environment =  {
-      systemPackages = with pkgs; [
-        waybar
-        wofi
-        hyprpaper
-        ];
-
-      sessionVariables = {
-        WLR_NO_HARDWARE_CURSORS = "1";
+  config = mkMerge [
+    (mkIf cfg.enableHyprland {
+      evilcfg.desktop = true;
+      programs.hyprland = {
+        enable = true;
+        xwayland.enable = true;
       };
-    };
-  };
+
+      security = {
+        polkit.enable = true;
+      };
+
+      environment =  {
+        systemPackages = with pkgs; [
+          hyprpaper
+        ];
+      };
+    })
+
+    (mkIf (! cfg.enableGnome) {
+      xdg.portal = {
+        enable = true;
+        extraPortals = with pkgs; [
+          xdg-desktop-portal
+          xdg-desktop-portal-gtk
+          xdg-desktop-portal-gnome
+        ];
+      };
+
+      environment.systemPackages = with pkgs.gnome; [
+        pkgs.cantarell-fonts
+        pkgs.loupe
+        adwaita-icon-theme
+        nautilus
+        baobab
+        gnome-calendar
+        gnome-boxes
+        gnome-system-monitor
+        gnome-control-center
+        gnome-weather
+        gnome-calculator
+        gnome-clocks
+        gnome-software # for flatpak
+      ];
+
+      systemd = {
+        user.services.polkit-gnome-authentication-agent-1 = {
+          description = "polkit-gnome-authentication-agent-1";
+          wantedBy = [ "graphical-session.target" ];
+          wants = [ "graphical-session.target" ];
+          after = [ "graphical-session.target" ];
+          serviceConfig = {
+            Type = "simple";
+            ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+            Restart = "on-failure";
+            RestartSec = 1;
+            TimeoutStopSec = 10;
+          };
+        };
+      };
+
+      services = {
+        devmon.enable = true;
+        udisks2.enable = true;
+        upower.enable = true;
+        power-profiles-daemon.enable = true;
+        accounts-daemon.enable = true;
+        gnome = {
+          sushi.enable = true;
+          evolution-data-server.enable = true;
+          glib-networking.enable = true;
+          gnome-keyring.enable = true;
+          gnome-online-accounts.enable = true;
+        };
+      };
+    })
+  ];
 }
