807a34032f1362f4432ec13a8dd103b0230f4037
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2024-04-08 09:30:57 +0000

    Add evilbook-hardware-configuration.nix for evilbook host

--- hosts/evilbook/default.nix
+++ hosts/evilbook/default.nix
@@ -4,6 +4,7 @@
 }: {
   imports = [
     ./evilbook-partition-scheme.nix
+    ./evilbook-hardware-configuration.nix
     ./evilbook-configuration.nix
     ./evilbook-home.nix
     ./evilbook-home-nvim.nix
