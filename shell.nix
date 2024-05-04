daa3bd123f495a74c3ac81697542bf26df0c4a3d
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-05-04 21:21:24 +0200

    refactor shell file

--- shell.nix
+++ shell.nix
@@ -1,16 +1,28 @@
-{
-  pkgs ? (import ./nixpkgs.nix) { }, ...
-}:
-{
+{ pkgs ? # If pkgs is not defined, instantiate nixpkgs from locked commit
+  let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs-unstable.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in
+  import nixpkgs { overlays = [ ]; }
+, ...
+}: {
   default = pkgs.mkShell {
-    name = "evilwoods-nixos-config";
-    NIX_CONFIG = "experimental-features = nix-command flakes";
+    name = "evilwoods-nixos-config-env";
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
     nativeBuildInputs = with pkgs; [
       fish
       nix
       home-manager
       git
+      just
+      age
+      ssh-to-age
+      sops
     ];
+
     shellHook = ''
       exec fish
     '';
