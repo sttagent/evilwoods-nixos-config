e35be8406e99a8a474b2dcb893b450ac816ab9cc
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-05-06 11:49:33 +0200

    login to tailscale

--- modules/base/shared-configuration.nix
+++ modules/base/shared-configuration.nix
@@ -62,11 +62,14 @@ with lib; {
 
 
     # tailscale configuration
+    sops.secrets.tailscale-auth-key = {};
     networking.firewall.trustedInterfaces = [ "tailscale0" ];
     services = {
       tailscale = {
         enable = true;
         useRoutingFeatures = "client";
+        extraUpFlags = [ "--ssh" "--operator=${mainUser}" ];
+        authKeyFile = config.sops.secrets.tailscale-auth-key.path;
       };
     };
 
