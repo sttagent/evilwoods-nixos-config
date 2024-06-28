075b9e4bfa9d44cef759444d6d5ef0c413fcca8f
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-06-29 00:31:17 +0200

    remove mount

--- hosts/evilcloud/evilcloud-configuration.nix
+++ hosts/evilcloud/evilcloud-configuration.nix
@@ -4,7 +4,7 @@
   # evilwoods.docker = true;
   # evilwoods.libvirtd = true;
 
-  networking.hostName = thisHost.hostname;
+  networking.hostName = "${thisHost.hostname}";
 
   boot = {
     initrd = {
@@ -23,16 +23,7 @@
     enable = true;
   };
 
-  fileSystems."/home/aitvaras/Storage" = {
-    device = "/dev/disk/by-partlabel/disk-data-home";
-    fsType = "btrfs";
-    options = [ "subvol=storage" "compress=1" ];
-  };
-
   # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
-
-  hardware.enableRedistributableFirmware = lib.mkDefault true;
-  # Use the systemd-boot EFI boot loader.
 
   # Version of NixOS installed from live disk. Needed for backwards compatability.
   system.stateVersion = "24.05"; # Did you read the comment?
