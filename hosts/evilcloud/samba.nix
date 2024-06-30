8f029e32c7870bb36c830883ec42830b04f74ba5
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-06-30 15:24:44 +0200

    add ryne private sambe folder

--- hosts/evilcloud/samba.nix
+++ hosts/evilcloud/samba.nix
@@ -38,6 +38,16 @@
         "directory mask" = "0755";
         "force user" = "aitvaras";
       };
+      ryne = {
+        path = "/srv/external-backup/shares/private/ryne";
+        browseable = "yes";
+        "valid users" = "ryne";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "ryne";
+      };
     };
   };
 
