0e98cfb64f9a84da2d5c1e2f581e6882561cca77
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-06-30 22:32:02 +0200

    fix formating

--- configfiles/nvim/addon-nvim-treesitter.lua
+++ configfiles/nvim/addon-nvim-treesitter.lua
@@ -3,18 +3,18 @@ vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.opt.foldenable = false
 
 require('nvim-treesitter.configs').setup({
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
 
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "gnn",     -- set to `false` to disable one of the mappings
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-        },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",       -- set to `false` to disable one of the mappings
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
     },
+  },
 })
