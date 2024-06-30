935f21936aa51bc278d4ff6e148a54acfe9df8d1
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-06-30 22:30:42 +0200

    add ntfy service

--- hosts/evilcloud/default.nix
+++ hosts/evilcloud/default.nix
@@ -12,5 +12,6 @@
 
     # Services
     ./samba.nix
+    ./ntfy.nix
   ];
 }
---
+++ hosts/evilcloud/ntfy.nix
@@ -0,0 +1,9 @@
+{
+  services.ntfy-sh = {
+    enable = true;
+    settings = {
+      listen-http = ":8080";
+      base-url = "https://evilcloud";
+    };
+  };
+}
