0103258653a404e4fded345f97b131b5f123d4c1
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-07-16 23:11:37 +0200

    remove main user module config

--- modules/base/main-user.nix
+++ modules/base/main-user.nix
@@ -11,18 +11,4 @@ in
     default = "aitvaras";
     description = "The primary user of evilwoods.";
   };
-
-  config = {
-    /*  users.users = {
-      ${mainUser} = {
-        isNormalUser = true;
-        description = "Arvydas Ramanauskas";
-        hashedPassword = "$6$r5XosfFY6X0yH0kg$sPHtZm25ZWpsx86cdKUsjr8fMv6AU6Jmj26H9qBbRKVOJx5SUBw2sSIwXx5FxAvarWmukal0r7.Biy1wpClwd1";
-        extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-        shell = pkgs.fish;
-      };
-    }; */
-  };
-
-
 }
