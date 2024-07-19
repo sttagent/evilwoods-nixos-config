caf4e5839a14599421c08ca2d03b440fbaebffdd
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-07-19 23:04:14 +0200

    mount backup partition

--- hosts/evilcloud/evilcloud-configuration.nix
+++ hosts/evilcloud/evilcloud-configuration.nix
@@ -22,5 +22,13 @@
     backend = "podman";
   };
 
+  fileSystems = {
+    "/var/backups" = {
+      device = "/dev/disk/by-label/External-backup";
+      fsType = "btrfs";
+      options = [ "defaults" "noatime" "compress=zstd" ];
+    };
+  };
+
   # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
 }
