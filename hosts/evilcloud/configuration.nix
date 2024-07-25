875bb9f20fd389bc917280e4404c9344e4300d30
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-07-25 23:51:04 +0200

    remove old boot loader config

--- hosts/evilcloud/configuration.nix
+++ hosts/evilcloud/configuration.nix
@@ -5,17 +5,6 @@
     initrd = {
       verbose = false;
     };
-
-    loader = {
-      efi = {
-        canTouchEfiVariables = true;
-      };
-      systemd-boot = {
-        enable = true;
-        configurationLimit = 10;
-      };
-    };
-
   };
 
   # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
