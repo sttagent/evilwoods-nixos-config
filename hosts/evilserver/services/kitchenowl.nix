999f55f6442f24dd0a49c4beac4237704b7add64
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-07-27 00:59:20 +0200

    fix typo

--- hosts/evilserver/services/kitchenowl.nix
+++ hosts/evilserver/services/kitchenowl.nix
@@ -3,7 +3,7 @@
     kitchenowl-app = {
       autoStart = true;
       image = "docker.io/tombursch/kitchenowl:v0.5.2";
-      ports = [ "8081e8080" ];
+      ports = [ "8081:8080" ];
       environment = {
         JWT_SECRET_KEY = "satan_spawn";
       };
