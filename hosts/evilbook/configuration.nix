54aa400f2a3f9f6e20850755661ee7f780b83566
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-07-28 20:28:29 +0200

    enable libvirtd service

--- hosts/evilbook/configuration.nix
+++ hosts/evilbook/configuration.nix
@@ -63,6 +63,21 @@
     enable = true;
   };
 
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
 
   # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
 }
