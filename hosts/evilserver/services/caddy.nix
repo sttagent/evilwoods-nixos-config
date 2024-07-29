d46a3e6e2b2f8380b8ea39c5b90db310e2610b9f
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-07-29 20:42:36 +0200

    use acme certificates

--- hosts/evilserver/services/caddy.nix
+++ hosts/evilserver/services/caddy.nix
@@ -3,8 +3,11 @@
     enable = true;
     virtualHosts = {
       "kitchenowl.evilwoods.net".extraConfig = ''
+        tls /var/lib/acme/evilwoods.net/cert.pem /var/lib/acme/evilwoods.net/key.pem
         reverse_proxy http://127.0.0.1:8081
       '';
     };
   };
+
+  users.users.caddy.extraGroups = [ "acme" ];
 }
