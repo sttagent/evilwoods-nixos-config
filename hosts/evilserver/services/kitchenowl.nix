a16968e9146ffd259c331c599536106e0a5ae23a
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-07-31 01:53:54 +0200

    add backup service to ext hdd for kitchenowl

--- hosts/evilserver/services/kitchenowl.nix
+++ hosts/evilserver/services/kitchenowl.nix
@@ -1,6 +1,15 @@
-{
+{ pkgs, ... }:
+let
+  appName = "kitchenowl-app";
+  appVolume = "kitchenowl_data";
+  dockeServiceName = "docker-${appName}";
+  rsyncServiceName = "rsync-${appName}";
+  snapshotServiceName = "snapshot-${appName}";
+  srcPath = "/var/snapshots/${appName}/docker/volumes/${appVolume}";
+  dstPath = "/var/backups/docker/volumes/${appVolume}";
+in {
   virtualisation.oci-containers.containers = {
-    kitchenowl-app = {
+    ${appName} = {
       autoStart = true;
       image = "docker.io/tombursch/kitchenowl:v0.5.2";
       ports = [ "8081:8080" ];
@@ -8,8 +17,60 @@
         JWT_SECRET_KEY = "satan_spawn";
       };
       volumes = [
-        "kitchenowl_data:/data"
+        "${appVolume}:/data"
       ];
     };
   };
+
+  systemd.timers."${snapshotServiceName}" = {
+    enable = true;
+    description = "${appName} backup timer";
+    wantedBy = [ "multi-user.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      AccuracySec = "1s";
+      RandomizedDelaySec = "5h";
+    };
+
+  };
+
+  systemd.services."${snapshotServiceName}" = {
+    enable = true;
+    description = "Take btrfs snapshot of storage subvol";
+    conflicts = [ "${dockeServiceName}.service" ];
+    after = [ "${dockeServiceName}.service" ];
+    onSuccess = [
+      "${dockeServiceName}.service"
+      "${rsyncServiceName}.service"
+    ];
+    onFailure = [
+      "${dockeServiceName}.service"
+    ];
+    unitConfig = {
+      RequiresMountsFor = "/var/storage /var/snapshots";
+    };
+    script = ''
+      snapshot="${appName}-$(${pkgs.coreutils-full}/bin/date +%F-%H-%M-%S)" && \
+      ${pkgs.btrfs-progs}/bin/btrfs subvolume snapshot -r /var/storage /var/snapshots/$snapshot && \
+      ln -s /var/snapshots/$snapshot /var/snapshots/${appName}
+    '';
+  };
+
+  systemd.services."${rsyncServiceName}" = {
+    enable = true;
+    description = "Syncs ${appName} data to backup drive.";
+    unitConfig = {
+      RequiresMountsFor = "/var/snapshots /var/backups";
+    };
+    script = ''
+      mkdir -p ${dstPath} && \
+      chmod 700 ${dstPath} && \
+      ${pkgs.rsync}/bin/rsync -a ${srcPath}/ ${dstPath} --delete
+    '';
+    postStop = ''
+      rm /var/snapshots/${appName}
+      ${pkgs.btrfs-progs}/bin/btrfs subvolume delete /var/snapshots/${appName}-*
+    '';
+  };
+
 }
