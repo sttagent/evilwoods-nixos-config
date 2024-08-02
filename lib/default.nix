4dc8fb73aea6885c45334dc4947bc435d1f6c94a
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-08-03 01:47:22 +0200

    add func that imports files in the same dir as default.nix

--- lib/default.nix
+++ lib/default.nix
@@ -1,9 +1,22 @@
 { lib, ... }:
-{
+rec {
   defaults = {
     defaultSystem = "x86_64-linux";
     mainUser = "aitvaras";
   };
+  
+  readFilterPath = filter: path: lib.filterAttrs filter (builtins.readDir path);
+  
+  
+  scanPathForImports = path:
+  let
+    filter = name: type:
+        if type == "directory" then
+            builtins.pathExists ./${name}/default.nix
+        else
+            (name != "default.nix") && lib.hasSuffix ".nix" name;
+  in
+  lib.mapAttrsToList (n: v: path + "/${n}") (readFilterPath filter path);
   
   # TODO write function to scan folder and include files
 }
