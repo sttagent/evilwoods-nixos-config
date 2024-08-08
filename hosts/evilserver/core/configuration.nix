98236fc9c9f036642dbf18c322f60bbaf5614f3c
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-08-08 10:38:14 +0200

    add missing pub key

--- hosts/evilserver/core/configuration.nix
+++ hosts/evilserver/core/configuration.nix
@@ -5,4 +5,7 @@
 
     inputs.disko-2405.nixosModules.disko
   ];
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFc8oFtu7i4WBlbcDMB7ua9cHJW2bzeomrLFddokw7v aitvaras@evilbook"
+  ];
 }
