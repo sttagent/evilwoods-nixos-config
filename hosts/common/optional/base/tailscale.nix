55e280e98455a4a29d67b92c83a675ec4622b1e2
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-08-11 13:34:14 +0200

    adapt tailscale conig to test env

--- hosts/common/optional/base/tailscale.nix
+++ hosts/common/optional/base/tailscale.nix
@@ -1,26 +1,35 @@
 { config, lib, ... }:
 let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf mkMerge;
 
   mainUser = config.evilwoods.mainUser;
   isTestEnv = config.evilwoods.isTestEnv;
 in
 {
-  # tailscale configuration
-  sops.secrets.tailscale-auth-key = { };
-  networking.firewall.trustedInterfaces = [ "tailscale0" ];
-  services = {
-    tailscale = {
-      enable = true;
-      useRoutingFeatures = "client";
-      extraUpFlags = [
-        "--ssh"
-        "--operator=${mainUser}"
-      ];
-      authKeyFile = config.sops.secrets.tailscale-auth-key.path;
-    };
-  };
-} // (mkIf isTestEnv {
-  sops.secrets.tailscale-ephemeral-auth-key = { };
-  services.tailscale.authKeyFile = lib.mkForce config.sops.secrets.tailscale-ephemeral-auth-key.path;
-})
+  config = mkMerge [
+    {
+      # tailscale configuration
+      networking.firewall.trustedInterfaces = [ "tailscale0" ];
+      services = {
+        tailscale = {
+          enable = true;
+          useRoutingFeatures = "client";
+          extraUpFlags = [
+            "--ssh"
+            "--operator=${mainUser}"
+          ];
+        };
+      };
+    }
+
+    (mkIf (!isTestEnv) {
+      sops.secrets.tailscale-auth-key = { };
+      services.tailscale.authKeyFile = config.sops.secrets.tailscale-auth-key.path;
+    })
+
+    (mkIf isTestEnv {
+      sops.secrets.tailscale-ephemeral-auth-key = { };
+      services.tailscale.authKeyFile = config.sops.secrets.tailscale-ephemeral-auth-key.path;
+    })
+  ];
+}
