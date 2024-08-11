9833d37f20bef1103a09ebd2f0fb21331aab37c9
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-08-11 13:34:14 +0200

    adapt acme config to config with test env

--- hosts/evilserver/services/acme.nix
+++ hosts/evilserver/services/acme.nix
@@ -1,30 +1,43 @@
-{ inputs, config, ... }:
+{ config, lib, ... }:
 let
-  secretsPath = builtins.toString inputs.evilsecrets;
+  inherit (lib) mkIf mkMerge;
+
+  isTestEnv = config.evilwoods.isTestEnv;
 in
 {
-  sops.secrets."acme-cloudflare.env" = {
-    mode = "0400";
-    owner = config.users.users.acme.name;
-    group = config.users.users.acme.group;
-  };
-
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "acme.kpo9e@ewmail.me";
-    certs = {
-      "evilwoods.net" = {
-        domain = "evilwoods.net";
-        extraDomainNames = [ "*.evilwoods.net" ];
-        dnsProvider = "cloudflare";
-        dnsPropagationCheck = true;
-        environmentFile = config.sops.secrets."acme-cloudflare.env".path;
-        reloadServices = [ "caddy.service" ];
-        postRun = ''
-          scp -pr . root@evilcloud:/var/lib/acme/
-          ssh -t root@evilcloud "chown -R acme: /var/lib/acme/evilwoods.net && systemctl restart caddy.service"
-        '';
+  config = mkMerge [
+    {
+      sops.secrets."acme-cloudflare.env" = {
+        mode = "0400";
+        owner = config.users.users.acme.name;
+        group = config.users.users.acme.group;
       };
-    };
-  };
+
+      security.acme = {
+        acceptTerms = true;
+        defaults.email = "acme.kpo9e@ewmail.me";
+        certs = {
+          "evilwoods.net" = {
+            domain = "evilwoods.net";
+            extraDomainNames = [ "*.evilwoods.net" ];
+            dnsProvider = "cloudflare";
+            dnsPropagationCheck = true;
+            environmentFile = config.sops.secrets."acme-cloudflare.env".path;
+            reloadServices = [ "caddy.service" ];
+          };
+        };
+      };
+    }
+
+    (mkIf (!isTestEnv) {
+      security.acme.certs."evilwoods.net".postRun = ''
+        scp -pr . root@evilcloud:/var/lib/acme/
+        ssh -t root@evilcloud "chown -R acme: /var/lib/acme/evilwoods.net && systemctl restart caddy.service"
+      '';
+    })
+
+    (mkIf isTestEnv {
+      security.acme.certs."evilwoods.net".server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+    })
+  ];
 }
