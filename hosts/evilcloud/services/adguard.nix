3996fbd3bf520c6d04bfd197632e928e99ecbb2f
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-08-23 13:12:11 +0200

    add more filters

--- hosts/evilcloud/services/adguard.nix
+++ hosts/evilcloud/services/adguard.nix
@@ -19,6 +19,68 @@
         ];
         refuse_any = true;
       };
+      filters = [
+        {
+          name = "AdGuard DNS filter";
+          enabled = true;
+          id = 1;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt";
+        }
+        {
+          name = "AdGuard Default Blocklist";
+          enabled = true;
+          id = 2;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_2.txt";
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_12.txt";
+          name = "Dandelion Sprout's Anti-Malware List";
+          idi = 1724512503;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt";
+          name = "Malicious URL Blocklist (URLHaus)";
+          id = 1724512504;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_50.txt";
+          name = "uBlock₀ filters – Badware risks";
+          id = 1724512505;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_4.txt";
+          name = "Dan Pollock's List";
+          id = 1724512506;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_3.txt";
+          name = "Peter Lowe's Blocklist";
+          id = 1724512507;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_18.txt";
+          name = "Phishing Army";
+          id = 1724512508;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_33.txt";
+          name = "Steven Black's List";
+          id = 1724512509;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_30.txt";
+          name = "Phishing URL Blocklist (PhishTank and OpenPhish)";
+          id = 1724512510;
+        }
+      ];
       filtering = {
         protection_enabled = true;
         filtering_enabled = true;
