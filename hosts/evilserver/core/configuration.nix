e034563eb897d6dc35bcd6e380161df15092a20b
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-09-01 01:06:34 +0200

    configure ip address

--- hosts/evilserver/core/configuration.nix
+++ hosts/evilserver/core/configuration.nix
@@ -8,6 +8,20 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFc8oFtu7i4WBlbcDMB7ua9cHJW2bzeomrLFddokw7v aitvaras@evilbook"
   ];
+  
+  networking = {
+    useDHCP = false;
+    defaultGateway = "192.168.2.1";
+    interfaces.enp2s0= {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "192.168.2.2";
+          prefixLength = 24;
+        }
+      ];
+    };
+  };
 
   system.stateVersion = "24.05";
 }
