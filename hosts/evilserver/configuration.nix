eb1668c952784f4fe67d0441c59ecfbf54ca520a
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-09-27 12:13:06 +0200

    fix filesystem mounting

--- hosts/evilserver/configuration.nix
+++ hosts/evilserver/configuration.nix
@@ -3,6 +3,13 @@
   configPath,
   ...
 }:
+let
+  common_mount_options = [
+    "noatime"
+    "defaults"
+    "compress=zstd"
+  ];
+in
 {
   imports = [
     (configPath + "/common")
@@ -32,17 +39,43 @@
     };
   };
 
-  # fileSystems = {
-  #   "/var/backups" = {
-  #     device = "/dev/disk/by-label/External-backup";
-  #     fsType = "btrfs";
-  #     options = [
-  #       "defaults"
-  #       "noatime"
-  #       "compress=zstd"
-  #     ];
-  #   };
-  # };
+  fileSystems = {
+    "/var/storage/external-hdd/backups" = {
+      device = "/dev/disk/by-label/external-hdd";
+      fsType = "btrfs";
+      options = [
+        "subvol=backups"
+      ] ++ common_mount_options;
+    };
+    "/var/storage/external-hdd/snapshots" = {
+      device = "/dev/disk/by-label/external-hdd";
+      fsType = "btrfs";
+      options = [
+        "subvol=snapshots"
+      ] ++ common_mount_options;
+    };
+    "/var/lib/containers" = {
+      device = "/dev/disk/by-label/internal-ssd";
+      fsType = "btrfs";
+      options = [
+        "subvol=containers/system"
+      ] ++ common_mount_options;
+    };
+    "/var/storage/internal-ssd/storage" = {
+      device = "/dev/disk/by-label/internal-ssd";
+      fsType = "btrfs";
+      options = [
+        "subvol=storage"
+      ] ++ common_mount_options;
+    };
+    "/var/storage/internal-ssd/snapshots" = {
+      device = "/dev/disk/by-label/internal-ssd";
+      fsType = "btrfs";
+      options = [
+        "subvol=snapshots"
+      ] ++ common_mount_options;
+    };
+  };
 
   evilwoods = {
     rsyncBackup.defaults = {
