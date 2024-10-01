c1f107a7d10b313017b76c9ca628c413b4bf7945
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-10-01 23:09:10 +0200

    configure nextcloud instance

--- hosts/evilserver/services/caddy.nix
+++ hosts/evilserver/services/caddy.nix
@@ -7,48 +7,74 @@
       }
     '';
     virtualHosts = {
-      "*.evilwoods.net".extraConfig = ''
-        tls /var/lib/acme/evilwoods.net/cert.pem /var/lib/acme/evilwoods.net/key.pem
-
-        @kitchenowl host kitchenowl.evilwoods.net
-        handle @kitchenowl {
-          # forward_auth 127.0.0.1:9091 {
-          #   uri /api/authz/forward-auth
-          #   copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
-          # }
-          reverse_proxy http://127.0.0.1:8081
-        }
-
-        @auth host auth.evilwoods.net
-        handle @auth {
-          reverse_proxy 127.0.0.1:9091
-        }
-
-        @lt host lt.evilwoods.net
-        handle @lt {
-          reverse_proxy 127.0.0.1:8010
-        }
-
-        @dns host dns.evilwoods.net
-        handle @dns {
-            forward_auth 127.0.0.1:9091 {
-            uri /api/authz/forward-auth
-            copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
+      "*.evilwoods.net" = {
+        useACMEHost = "evilwoods.net";
+        extraConfig = ''
+          @kitchenowl host kitchenowl.evilwoods.net
+          handle @kitchenowl {
+            # forward_auth 127.0.0.1:9091 {
+            #   uri /api/authz/forward-auth
+            #   copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
+            # }
+            reverse_proxy http://127.0.0.1:8082
           }
-          reverse_proxy 192.168.1.3:3000
-        }
 
-        @nextcloud host nextcloud.evilwoods.net
-        handle @nextcloud {
-          reverse_proxy 127.0.0.1:8080
-        }
+          @auth host auth.evilwoods.net
+          handle @auth {
+            reverse_proxy 127.0.0.1:9091
+          }
 
-        handle {
-          abort
-        }
-      '';
+          @lt host lt.evilwoods.net
+          handle @lt {
+            reverse_proxy 127.0.0.1:8010
+          }
+
+          @dns host dns.evilwoods.net
+          handle @dns {
+              forward_auth 127.0.0.1:9091 {
+              uri /api/authz/forward-auth
+              copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
+            }
+            reverse_proxy 192.168.1.3:3000
+          }
+
+          @nextcloud host nextcloud.evilwoods.net
+          handle @nextcloud {
+            encode gzip
+            reverse_proxy 127.0.0.1:8081 {
+              header_up X-Forwarded-For {remote_host}
+              header_up X-Forwarded-Proto {scheme}
+              header_up X-Forwarded-host {host}
+            }
+          }
+
+          handle {
+            abort
+          }
+        '';
+      };
+      "nextcloud.evilwoods.net:8080" = {
+        extraConfig = ''
+          redir https://{host}{uri}
+        '';
+      };
+      "nextcloud.evilwoods.net:8443" = {
+        useACMEHost = "evilwoods.net";
+        extraConfig = ''
+          encode gzip
+          reverse_proxy 127.0.0.1:8081 {
+            header_up X-Forwarded-For {remote_host}
+            header_up X-Forwarded-Proto {scheme}
+            header_up X-Forwarded-host {host}
+          }
+        '';
+      };
     };
   };
 
   users.users.caddy.extraGroups = [ "acme" ];
+  networking.firewall.allowedTCPPorts = [
+    8080
+    8443
+  ];
 }
--- hosts/evilserver/services/nextcloud.nix
+++ hosts/evilserver/services/nextcloud.nix
@@ -1,39 +1,94 @@
-{ config, pkgs, ... }:
 {
+  inputs,
+  config,
+  pkgs,
+  ...
+}:
+let
+  sopsFile = builtins.toString (inputs.evilsecrets + "/secrets/nextcloud.yaml");
+in
+{
+  sops.secrets.admin-pass = {
+    inherit sopsFile;
+    mode = "0400";
+    owner = "nextcloud";
+    group = "nextcloud";
+  };
+
+  sops.secrets.cloudflared-creds = {
+    sopsFile = builtins.toString (inputs.evilsecrets + "/secrets/cloudflared.yaml");
+    owner = "cloudflared";
+    group = "cloudflared";
+    mode = "0600";
+  };
+
+  sops.secrets.cloudflared-tunnel-creds = {
+    sopsFile = builtins.toString (inputs.evilsecrets + "/secrets/cloudflared.yaml");
+    owner = "cloudflared";
+    group = "cloudflared";
+    mode = "0600";
+  };
+
   services.nextcloud = {
     enable = true;
+    https = true;
     package = pkgs.nextcloud29;
     hostName = "localhost";
     home = "/var/storage/internal-ssd/storage/nextcloud";
     database.createLocally = true;
     config = {
-      adminpassFile = "/etc/nextcloud/adminPassword";
+      adminpassFile = config.sops.secrets.admin-pass.path;
+      adminuser = "admin";
       dbtype = "pgsql";
     };
-    settings.trusted_domains = [ "nextcloud.evilwoods.net" ];
+    settings = {
+      trusted_domains = [ "nextcloud.evilwoods.net" ];
+      trusted_proxies = [ "127.0.0.1" ];
+      maintenance_window_start = 1;
+      log_type = "file";
+    };
     extraAppsEnable = true;
     extraApps = {
-      inherit (config.services.nextcloud.package.packages.apps) calendar;
+      inherit (config.services.nextcloud.package.packages.apps)
+        calendar
+        tasks
+        notes
+        contacts
+        ;
     };
   };
 
-  services.nginx.virtualHosts.localhost.listen = [
-    {
-      addr = "127.0.0.1";
-      port = 8080;
-    }
-  ];
+  services.nginx.virtualHosts.localhost = {
+    listen = [
+      {
+        addr = "127.0.0.1";
+        port = 8081;
+      }
+    ];
+  };
 
   systemd.tmpfiles.rules = [
     "d /var/storage/internal-ssd/storage/nextcloud 0775 nextcloud nextcloud"
   ];
 
-  environment.etc."nextcloud/adminPassword" = {
-    text = ''
-      password
-    '';
-    user = "nextcloud";
-    group = "nextcloud";
-    mode = "0440";
+  environment.etc."cloudflared/cert.pem" = {
+    source = config.sops.secrets.cloudflared-creds.path;
+    user = "cloudflared";
+    group = "cloudflared";
+    mode = "0600";
   };
+
+  services.cloudflared = {
+    enable = true;
+    tunnels = {
+      "957f3219-6aa5-4672-9d53-2377b59cc308s" = {
+        credentialsFile = config.sops.secrets.cloudflared-tunnel-creds.path;
+        default = "http_status:404";
+        ingress = {
+          "nextcloud.evilwoods.net" = "http://127.0.0.1:8081";
+        };
+      };
+    };
+  };
+
 }
