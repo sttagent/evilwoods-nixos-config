6f8876d13747a01aeec02ab78a8a5fe46f8e759a
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-10-09 10:26:30 +0200

    remove supermaven from cmp sources

--- configfiles/lazynvim/lua/plugins/supermaven.lua
+++ configfiles/lazynvim/lua/plugins/supermaven.lua
@@ -1,38 +1,9 @@
 return {
-  {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({
-        disable_inline_completion = true,
-      })
-    end,
-  },
-
-  {
-    "nvim-cmp",
-    dependencies = {
-      {
+    {
         "supermaven-inc/supermaven-nvim",
-        opts = {},
-      },
+        config = function()
+            require("supermaven-nvim").setup({
+            })
+        end,
     },
-
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "supermaven",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
-  },
-
-  {
-    "nvim-lualine/lualine.nvim",
-    optional = true,
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("supermaven"))
-    end,
-  }
 }
