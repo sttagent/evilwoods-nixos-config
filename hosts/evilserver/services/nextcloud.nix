378876f09d11c022de3aedb858256b16c5aafab8
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-10-09 12:49:56 +0200

    enable preview generation

--- hosts/evilserver/services/nextcloud.nix
+++ hosts/evilserver/services/nextcloud.nix
@@ -48,6 +48,21 @@ in
       trusted_domains = [ "nextcloud.evilwoods.net" ];
       trusted_proxies = [ "127.0.0.1" ];
       maintenance_window_start = 1;
+      enable_previews = true;
+      enabledPreviewProviders = [
+        "OC\\Preview\\Movie"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\MP4"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\PDF"
+      ];
+
     };
     extraAppsEnable = true;
     extraApps = {
