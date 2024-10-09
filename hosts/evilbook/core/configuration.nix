2c391d79121241a1c8c061de03d31e09c0e721e6
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-10-09 12:49:56 +0200

    use new user gneneration service

--- hosts/evilbook/core/configuration.nix
+++ hosts/evilbook/core/configuration.nix
@@ -1,10 +1,19 @@
-{ inputs, configPath, ... }:
+{
+  inputs,
+  configPath,
+  modulesPath,
+  ...
+}:
 {
   imports = [
     (configPath + "/hardware/boot/systemd-boot.nix")
     (configPath + "/core")
+    # (modulesPath + "/profiles/perlless.nix")
     inputs.disko.nixosModules.disko
   ];
+
+  services.userborn.enable = true;
+  # system.etc.overlay.enable = true;
 
   system.stateVersion = "24.05";
 }
