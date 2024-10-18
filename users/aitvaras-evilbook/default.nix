09304149f7a3d9d7b663123ff06b01d21ed86b40
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-10-18 10:01:23 +0200

    configure zed edior

--- users/aitvaras-evilbook/default.nix
+++ users/aitvaras-evilbook/default.nix
@@ -154,6 +154,56 @@ in
         };
       };
 
+      zed-editor = {
+        enable = true;
+        extensions = [
+          "nix"
+          "just"
+          "lua"
+          "toml"
+        ];
+        userSettings = {
+          theme = {
+            mode = "system";
+            light = "Gruvbox Light Hard";
+            dark = "Gruvbox Dark Hard";
+          };
+          features = {
+            inline_completion_provider = "supermaven";
+          };
+          assistant = {
+            default_model = {
+              provider = "zed.dev";
+              model = "claude-3-5-sonnet-20240620";
+            };
+            version = 2;
+          };
+          autosave = "on_focus_change";
+          vim_mode = true;
+          vim = {
+            toggle_relative_line_numbers = true;
+          };
+          ui_font_size = 16;
+          buffer_font_family = "CommitMono Nerd Font";
+          buffer_font_size = 15;
+          buffer_line_height = "standard";
+          tabs = {
+            file_icons = true;
+            git_status = true;
+          };
+          languages = {
+            Nix = {
+              format_on_save = "on";
+              tab_size = 2;
+            };
+          };
+          terminal = {
+            shell = "xonsh";
+            line_height = "standard";
+          };
+        };
+      };
+
       vscode = {
         enable = false;
         extensions = with pkgs.vscode-extensions; [
