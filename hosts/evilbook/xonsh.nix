e283af015d6e1242516de4beefeccaa468c0a7a4
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-10-18 10:01:23 +0200

    fix config

--- hosts/evilbook/xonsh.nix
+++ hosts/evilbook/xonsh.nix
@@ -50,13 +50,12 @@
         ];
       };
       config = ''
-        xontrib load direnv fish_completer
-
-        aliases["eza"] = "eza --icons --group-directories-first"
-        aliases["ls"] = "eza"
+        xontrib load coreutils direnv fish_completer
+        #
+        # aliases["ls"] = "eza"
         aliases["ll"] = "eza -l"
         aliases["lla"] = "eza -l -a"
-
+        #
         execx($(starship init xonsh))
         execx($(zoxide init xonsh), 'exec', __xonsh__.ctx, filename='zoxide')
         execx($(atuin init xonsh))
