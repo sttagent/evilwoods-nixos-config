d4b89ba52756b164e018b3f071a25f41e9a5dde8
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-10-25 10:34:49 +0200

    make android confi an option

--- modules/evilwoods/config/hardware/android.nix
+++ modules/evilwoods/config/hardware/android.nix
@@ -1,9 +1,20 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
+  inherit (lib) mkIf mkEnableOption;
   mainUser = config.evilwoods.vars.mainUser;
+  cfg = config.evilwoods.config.android;
 in
 {
-  users.users.${mainUser}.extraGroups = [ "adbusers" ];
-  programs.adb.enable = true;
-  services.udev.packages = [ pkgs.android-udev-rules ];
+  options.evilwoods.config.android.enable = mkEnableOption "android config";
+
+  config = mkIf cfg.enable {
+    users.users.${mainUser}.extraGroups = [ "adbusers" ];
+    programs.adb.enable = true;
+    services.udev.packages = [ pkgs.android-udev-rules ];
+  };
 }
