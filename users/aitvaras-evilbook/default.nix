65cab08d6cad8ca5388204557166a2bfa3e08a4e
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-01-25 16:36:08 +0100

    fix wiki url

--- users/aitvaras-evilbook/default.nix
+++ users/aitvaras-evilbook/default.nix
@@ -252,7 +252,7 @@ in
               };
 
               "NixOS Wiki" = {
-                urls = [ { template = "https://wiki.nixos.org/index.php?search={searchTerms}"; } ];
+                urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
                 iconUpdateURL = "https://wiki.nixos.org/favicon.png";
                 updateInterval = 24 * 60 * 60 * 1000; # every day
                 definedAliases = [ "@nw" ];
