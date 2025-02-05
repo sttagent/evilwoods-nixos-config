c6ee4237463d5b027991c8f62c8a038de96ce06a
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-02-05 19:17:35 +0100

    enable vscode mode

--- dotfiles/lazynvim/lua/config/lazy.lua
+++ dotfiles/lazynvim/lua/config/lazy.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
   },
