3c1095eb7a978be370471786197daeecb3eceec2
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-02-11 18:55:10 +0100

    add storage disk to config

--- hosts/evilcloud/partitions.nix
+++ hosts/evilcloud/partitions.nix
@@ -1,42 +1,62 @@
 { inputs, ... }:
 let
-  disks = [ "/dev/vda" ];
+  disks = [
+    "/dev/vda"
+    "/dev/vdb"
+  ];
 in
 {
   imports = [
     inputs.disko-2411.nixosModules.disko
   ];
   disko.devices = {
-    disk.nixos = {
-      device = builtins.elemAt disks 0;
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "1G";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
+    disk = {
+      nixos = {
+        device = builtins.elemAt disks 0;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
+            };
+            esp = {
+              name = "ESP";
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              name = "root";
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "pool";
+              };
             };
           };
-          root = {
-            name = "root";
-            size = "100%";
-            content = {
-              type = "lvm_pv";
-              vg = "pool";
-            };
-          };
+        };
+      };
+
+      block-storage = {
+        device = builtins.elemAt disks 1;
+        type = "disk";
+        content = {
+          type = "filesystem";
+          format = "xfs";
+          extraArgs = [ "-f" ];
+          mountpoint = "/var/storage";
+          mountOptions = [
+            "defaults"
+            "nofail"
+          ];
         };
       };
     };
