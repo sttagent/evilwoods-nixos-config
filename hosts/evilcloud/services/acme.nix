79acf961ae16afbdb8ceb4c564123e26e5d02ef0
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-02-11 14:33:19 +0100

    setup acme service

---
+++ hosts/evilcloud/services/acme.nix
@@ -0,0 +1,36 @@
+{ config, lib, ... }:
+let
+  inherit (lib) mkIf mkMerge;
+
+  isTestEnv = config.evilwoods.vars.isTestEnv;
+  listenHTTPPort = "1360";
+in
+{
+  config = mkMerge [
+    {
+      security.acme = {
+        acceptTerms = true;
+        defaults.email = "acme.kpo9e@ewmail.me";
+        certs = {
+          "evilwoods.net" = {
+            domain = "evilwoods.net";
+            listenHTTP = ":${listenHTTPPort}";
+            reloadServices = [ "caddy.service" ];
+          };
+        };
+      };
+
+      services.cloudflared.tunnels."5c5360ae-6911-41e2-bbcb-b5b78954349c" = {
+        ingress = {
+          "evilwoods.net" = "http://127.0.0.1:${listenHTTPPort}";
+          "search.evilwoods.net" = "http://127.0.0.1:${listenHTTPPort}";
+        };
+      };
+    }
+
+    (mkIf isTestEnv {
+      security.acme.certs."evilwoods.net".server =
+        "https://acme-staging-v02.api.letsencrypt.org/directory";
+    })
+  ];
+}
