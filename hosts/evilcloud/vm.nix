a2d8fe7da337f6aa5287fba982e2e16047b3c375
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-02-23 10:29:05 +0100

    add vmVarient cofig to hosts

---
+++ hosts/evilcloud/vm.nix
@@ -0,0 +1,22 @@
+# The file is not imported by default
+
+{ config, lib, ... }:
+let
+  inherit (lib) mkForce;
+in
+{
+  virtualisation.vmVariant = {
+    evilwoods.vars.isTestEnv = true;
+    networking.hostName = mkForce "${config.networking.hostName}-test";
+    virtualisation = {
+      memorySize = 1024 * 4;
+      cores = 2;
+      sharedDirectories = {
+        sshKeys = {
+          source = "/home/aitvaras/.config/evilwoods/ssh_keys/evilcloud";
+          target = "/etc/ssh";
+        };
+      };
+    };
+  };
+}
---
+++ hosts/evilserver/vm.nix
@@ -0,0 +1,23 @@
+# Configuration that is used in vm version of this nixos configuration
+# built by `nixos-rebuild build-vm`.
+
+{ config, lib, ... }:
+let
+  inherit (lib) mkForce;
+in
+{
+  virtualisation.vmVariant = {
+    evilwoods.vars.isTestEnv = true;
+    networking.hostName = mkForce "${config.networking.hostName}-test";
+    virtualisation = {
+      memorySize = 1024 * 4;
+      cores = 2;
+      sharedDirectories = {
+        sshKeys = {
+          source = "/home/aitvaras/.config/evilwoods/ssh_keys/evilserver";
+          target = "/etc/ssh";
+        };
+      };
+    };
+  };
+}
