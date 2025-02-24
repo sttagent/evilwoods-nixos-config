1d7e7e21ee9c3e4ac446b6fc5e54e38d53a58b5e
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-02-24 14:33:48 +0100

    fix evilbook vm config

--- hosts/evilbook/vm.nix
+++ hosts/evilbook/vm.nix
@@ -1,9 +1,20 @@
-# The file is not imported by default
-
 { config, lib, ... }:
+let
+  inherit (lib) mkForce;
+in
 {
-  imports = [ ./default.nix ];
-
-  sops.secrets.tailscale-ephemeral-auth-key = { };
-  services.tailscale.authKeyFile = lib.mkForce config.config.sops.tailscale-ephemeral-auth-key.path;
+  virtualisation.vmVariant = {
+    evilwoods.vars.isTestEnv = true;
+    networking.hostName = mkForce "${config.networking.hostName}-test";
+    virtualisation = {
+      memorySize = 1024 * 4;
+      cores = 2;
+      sharedDirectories = {
+        sshKeys = {
+          source = "/home/aitvaras/.config/evilwoods/ssh_keys/evilbook";
+          target = "/etc/ssh";
+        };
+      };
+    };
+  };
 }
