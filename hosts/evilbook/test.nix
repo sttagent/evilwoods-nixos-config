112ba44db386b5bd104d9a62e0f180dd7ef1bc9b
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-02-25 22:15:59 +0100

    remove tests

--- hosts/evilbook/test.nix
+++
@@ -1,9 +0,0 @@
-# The file is not imported by default
-
-{ config, lib, ... }:
-{
-  imports = [ ./default.nix ];
-
-  sops.secrets.tailscale-ephemeral-auth-key = { };
-  services.tailscale.authKeyFile = lib.mkForce config.config.sops.tailscale-ephemeral-auth-key.path;
-}
