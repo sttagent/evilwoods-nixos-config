00a5307c8366d4b78ddadb7b2c7bdca6b3743908
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-03-05 18:03:28 +0100

    fix ntfy servide

--- hosts/evilcloud/services/ntfy.nix
+++ hosts/evilcloud/services/ntfy.nix
@@ -3,27 +3,33 @@ let
   inherit (builtins) toString;
 
   inherit (config.evilwoods.host.vars) legoHTTPPort;
+  inherit (config.evilwoods.vars) domain;
+
+  appName = "ntfy";
+  appURL = "${appName}.${domain}";
+
+  ntfyListenPort = ":8086";
 in
 {
   services = {
     ntfy-sh = {
       enable = true;
       settings = {
-        listen-http = ":8086";
-        base-url = "https://ntfy.evilwoods.net";
+        listen-http = ntfyListenPort;
+        base-url = "https://${appURL}";
       };
     };
 
-    caddy.virtualHosts."ntfy.evilwoods.net" = {
-      useACMEHost = "ntfy.evilwoods.net";
+    caddy.virtualHosts."${appURL}" = {
+      useACMEHost = appURL;
       extraConfig = ''
-        reverse_proxy localhost:8086
+        reverse_proxy localhost${ntfyListenPort}
       '';
     };
   };
 
-  security.acme.certs."ntfy.evilwoods.net" = {
-    domain = "ntfy.evilwoods.net";
+  security.acme.certs."${appURL}" = {
+    domain = appURL;
     listenHTTP = ":${toString legoHTTPPort}";
     reloadServices = [ "caddy.service" ];
   };
