9604891f6a6da4decbbed0486e16426068973589
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-03-05 10:09:31 +0100

    fix cachix-agent services
    host is not needed. It default to right uri.

--- hosts/evilcloud/services/cachix-agent.nix
+++ hosts/evilcloud/services/cachix-agent.nix
@@ -5,7 +5,6 @@
   services = {
     cachix-agent = {
       enable = true;
-      host = "https://evilwoods.cachix.org";
       credentialsFile = config.sops.secrets.evilwoods-agent-token.path;
     };
   };
