0f80e66851b9f751058289ac96d2e620890ce7ad
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-03-30 15:51:18 +0200

    move desktop config to common config

--- hosts/common/desktop/configuration.nix
+++ hosts/common/desktop/configuration.nix
@@ -1,9 +1,94 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkIf;
+  inherit (config.evilwoods.vars) mainUser;
   isDesktop = config.evilwoods.vars.role == "desktop";
 in
 {
   config = mkIf isDesktop {
+    boot = {
+      kernelParams = [
+        "quiet"
+        "splash"
+      ];
+      plymouth.enable = true;
+    };
+
+    fonts.packages = with pkgs; [
+      font-awesome
+      nerd-fonts.commit-mono
+    ];
+
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+      extraLocaleSettings = {
+        LC_ADDRESS = "sv_SE.UTF-8";
+        LC_IDENTIFICATION = "sv_SE.UTF-8";
+        LC_MEASUREMENT = "sv_SE.UTF-8";
+        LC_MONETARY = "sv_SE.UTF-8";
+        LC_NAME = "sv_SE.UTF-8";
+        LC_NUMERIC = "sv_SE.UTF-8";
+        LC_PAPER = "sv_SE.UTF-8";
+        LC_TELEPHONE = "sv_SE.UTF-8";
+        LC_TIME = "sv_SE.UTF-8";
+      };
+    };
+
+    environment.variables = {
+      NIXOS_OZONE_WL = "1";
+    };
+
+    zramSwap.enable = true;
+
+    # Recommended for pipwire
+    security.rtkit.enable = true;
+    security.pam.services.${mainUser}.enableGnomeKeyring = true;
+
+    # Needs to be disabled if using pipwire
+    hardware = {
+      bluetooth.enable = true;
+    };
+
+    programs = {
+      xwayland.enable = true;
+      gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+      };
+    };
+
+    services = {
+      flatpak.enable = true;
+      fstrim.enable = true;
+      pcscd.enable = true;
+      # Enable the X11 windowing system.
+      xserver = {
+        enable = true;
+      };
+
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # jack.enable = true;
+      };
+
+      udev = {
+        packages = [ pkgs.yubikey-personalization ];
+        extraRules = ''
+          # Bitbox udev rules
+          SUBSYSTEM=="usb", SYMLINK+="bitbox02_%n", GROUP="plugdev", MODE="0664", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2403"
+          KERNEL=="hidraw*", SUBSYSTEM=="hidraw", SYMLINK+="bitbox02_%n", GROUP="plugdev", MODE="0664", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2403"
+          SUBSYSTEM=="usb", SYMLINK+="dbb%n", GROUP="plugdev", MODE="0664", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2402"
+          KERNEL=="hidraw*", SUBSYSTEM=="hidraw", SYMLINK+="dbbf%n", GROUP="plugdev", MODE="0664", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2402"
+        '';
+      };
+    };
   };
 }
---
+++ hosts/common/desktop/packages.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (config.evilwoods.vars) mainUser;
+  isDesktop = config.evilwoods.vars.role == "desktop";
+in
+{
+  config = mkIf isDesktop {
+  };
+}
