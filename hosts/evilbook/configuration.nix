2fa322799665b7da72769b69458922ac68c941bc
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-10-17 18:11:13 +0200

    move nfs share config to a separate file

--- hosts/evilbook/configuration.nix
+++ hosts/evilbook/configuration.nix
@@ -67,8 +67,6 @@
     };
 
     tmp.useTmpfs = true;
-
-    supportedFilesystems = [ "nfs" ];
   };
 
   system = {
@@ -132,36 +130,6 @@
 
     nix-ld = {
       enable = true;
-    };
-  };
-
-  fileSystems = {
-    "/mnt/nfs/aitvaras_share" = {
-      device = "nas.evilwoods.net:/mnt/storage/shares/aitvaras";
-      fsType = "nfs";
-      options = [
-        "x-systemd.automount"
-        "_netdev"
-      ];
-    };
-    "/mnt/nfs/video" = {
-      device = "nas.evilwoods.net:/mnt/storage/media/video";
-      fsType = "nfs";
-      options = [
-        "x-systemd.automount"
-        "_netdev"
-      ];
-    };
-  };
-
-  users.groups = {
-    nas_aitvaras_share = {
-      gid = 4000;
-      members = [ "aitvaras" ];
-    };
-    nas_media = {
-      gid = 4100;
-      members = [ "aitvaras" ];
     };
   };
 
---
+++ hosts/evilbook/shares.nix
@@ -0,0 +1,33 @@
+{
+  boot.extraSystemdUnitPaths = [ "nfs" ];
+
+  fileSystems = {
+    "/mnt/nfs/aitvaras_share" = {
+      device = "nas.evilwoods.net:/mnt/storage/shares/aitvaras";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "_netdev"
+      ];
+    };
+    "/mnt/nfs/video" = {
+      device = "nas.evilwoods.net:/mnt/storage/media/video";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "_netdev"
+      ];
+    };
+  };
+
+  users.groups = {
+    nas_aitvaras_share = {
+      gid = 4000;
+      members = [ "aitvaras" ];
+    };
+    nas_media = {
+      gid = 4100;
+      members = [ "aitvaras" ];
+    };
+  };
+}
