e7ab3396689080497c40515b73d6206bc544b1cb
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-11-14 14:00:39 +0100

    run build command in build func

--- scripts/evilosctl/__main__.py
+++ scripts/evilosctl/__main__.py
@@ -140,7 +140,7 @@ def diff_result_with_current(args: argparse.Namespace, is_remote: bool = False) 
 
 def build_nixos_system(args: argparse.Namespace, is_remote: bool = False) -> None:
     _ = run_command(
-        f"nixos-rebuild {args.subcommand} --flake .#{args.nixos_config} 2>&1 | nom",
+        f"nixos-rebuild build --flake .#{args.nixos_config} 2>&1 | nom",
         shell=True,
         capture_output=False,
     )
@@ -152,7 +152,7 @@ def build_nixos_system(args: argparse.Namespace, is_remote: bool = False) -> Non
 def apply_nixos_config(args: argparse.Namespace, is_remote: bool = False) -> None:
     build_nixos_system(args, is_remote)
     _ = run_command(
-        f"nixos-rebuild {args.subcommand} --ask-sudo-password --flake .#{args.nixos_config}",
+        f"nixos-rebuild {args.subcommand} --ask-sudo-password --flake .#{args.nixos_config} 2>&1 | nom",
         shell=True,
         capture_output=False,
     )
