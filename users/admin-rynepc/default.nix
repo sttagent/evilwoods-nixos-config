5fedfc5dae300095c80ffbf040370ff1791b35d2
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-11-21 12:58:17 +0100

    add admin-rynepc config

---
+++ users/admin-rynepc/default.nix
@@ -0,0 +1,76 @@
+{
+  evilib,
+  pkgs,
+  ...
+}:
+let
+  inherit (evilib) mkImportList;
+  inherit (evilib.readInVarFile ../admin/vars.toml) currentUser;
+  # inherit (options.evilwoods.vars) shell;
+in
+{
+  imports = [ ../admin ] ++ (mkImportList ./.);
+
+
+  home-manager.users.${currentUser} = {
+
+    programs = {
+      home-manager.enable = true;
+
+      ghostty = {
+        enable = true;
+        settings = {
+          font-family = "CommitMono Nerd Font Mono";
+          theme = "Miasma";
+          window-theme = "ghostty";
+          window-padding-x = 2;
+          adw-toolbar-style = "flat";
+          shell-integration = "fish";
+          command = "fish";
+        };
+      };
+
+      gh = {
+        enable = true;
+        extensions = with pkgs; [ gh-copilot ];
+        settings = {
+          git_protocol = "ssh";
+        };
+      };
+
+      jujutsu = {
+        enable = true;
+        settings = {
+          user = {
+            name = "Arvydas Ramanauskas";
+            email = "711261+sttagent@users.noreply.github.com";
+          };
+          ui = {
+            merge-editor = "meld";
+            default-command = [
+              "status"
+            ];
+            paginate = "never";
+            diff-formater = [
+              "difft"
+              "--color=always"
+              "$left"
+              "$right"
+            ];
+          };
+        };
+      };
+
+    };
+    xdg = {
+      configFile = {
+        # zed = {
+        #   source = dotFilesPath + "/zed";
+        #   recursive = true;
+        # };
+      };
+
+    };
+
+  };
+}
