29f96b231ed9b29c6fa8fc5ac94e88b9059c6ddf
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-11-21 12:58:17 +0100

    remove unused config

--- users/ryne-rynepc/default.nix
+++ users/ryne-rynepc/default.nix
@@ -29,117 +29,6 @@ in
         };
       };
 
-      kitty = {
-        enable = false;
-
-        shellIntegration = {
-          enableFishIntegration = true;
-        };
-        themeFile = "GruvboxMaterialDarkHard";
-        settings = {
-          shell = "xonsh";
-          font_family = "Commit Mono Nerd Font";
-          wayland_titlebar_color = "background";
-        };
-      };
-      helix = {
-
-        enable = false;
-        defaultEditor = false;
-        settings = {
-          theme = "gruvbox_dark_hard";
-          editor = {
-            cursorline = true;
-            bufferline = "multiple";
-            auto-save = true;
-            rulers = [ 120 ];
-            color-modes = true;
-            whitespace = {
-              render = "all";
-            };
-            indent-guides = {
-              render = true;
-            };
-            line-number = "relative";
-            lsp = {
-              display-messages = true;
-              display-inlay-hints = true;
-            };
-
-            cursor-shape = {
-              insert = "bar";
-              normal = "block";
-              select = "block";
-            };
-          };
-          keys.normal = {
-            space.space = "file_picker";
-            space.w = ":w";
-            space.q = ":q";
-            space.l = [
-              ":new"
-              ":insert-output lazygit"
-              ":buffer-close!"
-              ":redraw"
-            ];
-            esc = [
-              "collapse_selection"
-              "keep_primary_selection"
-            ];
-          };
-        };
-
-        languages = {
-          language-server.nixd = {
-            command = "nixd";
-          };
-          language = [
-            {
-              name = "nix";
-              indent = {
-                tab-width = 2;
-                unit = " ";
-              };
-              language-servers = [ "nixd" ];
-              auto-format = true;
-              roots = [ "flake.lock" ];
-              formatter.command = "nixpkgs-fmt";
-            }
-          ];
-        };
-      };
-
-      gh = {
-        enable = true;
-        extensions = with pkgs; [ gh-copilot ];
-        settings = {
-          git_protocol = "ssh";
-        };
-      };
-
-      jujutsu = {
-        enable = true;
-        settings = {
-          user = {
-            name = "Arvydas Ramanauskas";
-            email = "711261+sttagent@users.noreply.github.com";
-          };
-          ui = {
-            merge-editor = "meld";
-            default-command = [
-              "status"
-            ];
-            paginate = "never";
-            diff-formater = [
-              "difft"
-              "--color=always"
-              "$left"
-              "$right"
-            ];
-          };
-        };
-      };
-
     };
     xdg = {
       autostart = {
@@ -149,13 +38,7 @@ in
         ];
       };
       configFile = {
-        # zed = {
-        #   source = dotFilesPath + "/zed";
-        #   recursive = true;
-        # };
       };
-
     };
-
   };
 }
