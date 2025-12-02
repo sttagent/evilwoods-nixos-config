fe028495b712fb883d8349a4ca3fa13baf51330f
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-12-02 13:46:17 +0100

    clone secrets repo

--- scripts/setup-install-env.bash
+++ scripts/setup-install-env.bash
@@ -8,6 +8,16 @@ else
     exit
 fi
 
+if [[ ! -e ./evilwoods-nixos-config-secrets/.git ]]; then
+    git clone --depth 1 git@github.com:sttagent/evilwoods-nixos-config-secrets.git || {
+        echo "ERROR: failed to clone evilwoods-nixos-config-secrets repo."
+        exit 1
+    }
+else
+    echo "INFO: local copy of evilwoods-nixos-config repo already exists."
+    exit
+fi
+
 cd evilwoods-nixos-config
 
 exec nix develop ".#install_env"
