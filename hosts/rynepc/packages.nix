a1cf6531a0c536f7f63c7c4ef37873754ee8a9f2
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-12-04 13:17:19 +0100

    add package and share config to rynepc

---
+++ hosts/rynepc/packages.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    ffmpeg-full
+    appimage-run
+    distrobox
+    yubioath-flutter
+    wl-clipboard
+    discord
+    obsidian
+    ghostty
+    # spotify
+    claude-code
+
+    # apps specific to Gnome
+    notify-client
+    fragments
+    fractal
+  ];
+}
---
+++ hosts/rynepc/shares.nix
@@ -0,0 +1,39 @@
+{ config, ... }:
+let
+  inherit (config.evilwodos.vars) mainUser;
+in
+{
+  boot.extraSystemdUnitPaths = [ "nfs" ];
+
+  fileSystems = {
+    "/mnt/nfs/aitvaras_share" = {
+      device = "nas.evilwoods.net:/mnt/storage/shares/aitvaras";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "x-systemd.umount.force"
+        "_netdev"
+      ];
+    };
+    "/mnt/nfs/video" = {
+      device = "nas.evilwoods.net:/mnt/storage/media/video";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "x-systemd.umount.force"
+        "_netdev"
+      ];
+    };
+  };
+
+  users.groups = {
+    nas_aitvaras_share = {
+      gid = 4000;
+      members = [ "${mainUser}" ];
+    };
+    nas_media = {
+      gid = 4100;
+      members = [ "${mainUser}" ];
+    };
+  };
+}
