04d0dbbba66d9741df38a94711f3cc8a6b92ccfc
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-12-08 10:50:04 +0100

    include nvidia related config if not in test env

--- hosts/rynepc/hardware.nix
+++ hosts/rynepc/hardware.nix
@@ -5,40 +5,53 @@
   pkgs,
   ...
 }:
+let
+  inherit (lib) mkMerge mkIf;
+  inherit (config.evilwoods.vars) isTestEnv;
+in
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
+  config = mkMerge [
+    (mkIf (!isTestEnv) {
+      boot = {
+        initrd = {
+          availableKernelModules = [
+            "xhci_pci"
+            "ahci"
+            "usbhid"
+            "ums_realtek"
+            "usb_storage"
+            "sd_mod"
+            "sr_mod"
+          ];
+          kernelModules = [ ];
+        };
 
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "ahci"
-    "usbhid"
-    "ums_realtek"
-    "usb_storage"
-    "sd_mod"
-    "sr_mod"
-  ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-  boot.blacklistedKernelModules = [ "nouveau" ];
-  hardware = {
-    graphics = {
-      enable = true;
-      enable32Bit = true;
-      extraPackages = with pkgs; [
-        nvidia-vaapi-driver
-        libva-vdpau-driver
-        libvdpau-va-gl
+        kernelModules = [ "kvm-intel" ];
+        extraModulePackages = [ ];
+        blacklistedKernelModules = [ "nouveau" ];
+      };
+
+      hardware = {
+        graphics = {
+          enable = true;
+          enable32Bit = true;
+          extraPackages = with pkgs; [
+            nvidia-vaapi-driver
+            libva-vdpau-driver
+            libvdpau-va-gl
+          ];
+        };
+        nvidia = {
+          open = false; # see the note above
+        };
+        cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+      };
+      services.xserver.videoDrivers = [
+        "nvidia"
       ];
-    };
-    nvidia = {
-      open = false; # see the note above
-    };
-    cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  };
-  services.xserver.videoDrivers = [
-    "nvidia"
+    })
   ];
 }
