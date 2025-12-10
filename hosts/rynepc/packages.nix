89538eababfad1f9fea52df07814e1b125ab884a
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2025-12-10 08:39:39 +0100

    remove unnecessary packages

--- hosts/rynepc/packages.nix
+++ hosts/rynepc/packages.nix
@@ -3,14 +3,12 @@
   environment.systemPackages = with pkgs; [
     ffmpeg-full
     appimage-run
-    distrobox
     yubioath-flutter
     wl-clipboard
     discord
     obsidian
     ghostty
     # spotify
-    claude-code
 
     # apps specific to Gnome
     notify-client
