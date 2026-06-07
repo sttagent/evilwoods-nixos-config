b86626f9731bfb42f9347e49226335b3d34fe877
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2026-06-07 18:10:43 +0200

    remove old binary cache

--- modules/hosts/evilcloud/configuration.nix
+++ modules/hosts/evilcloud/configuration.nix
@@ -57,7 +57,6 @@
           allowed-users = [ "@wheel" ];
           substituters = [
             "https://nix-community.cachix.org/"
-            "https://evilwoods.cachix.org/"
           ];
           trusted-public-keys = [
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
