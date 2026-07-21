e736f1807c0eae842cab2435c0898bc73d8f745d
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2026-07-22 01:39:53 +0200

    remove python packages from nix dev env

--- modules/devshell.nix
+++ modules/devshell.nix
@@ -2,7 +2,7 @@
 {
   den.schema.flake-system.includes = [ den.aspects.nixos-devshell ];
 
-  den.aspects.nixos-devshell =  {
+  den.aspects.nixos-devshell = {
     devShells = { pkgs, ... }: {
       default = pkgs.mkShell {
         name = "evilwoods-nixos-config";
@@ -12,14 +12,6 @@
           nvd
           nixfmt
           jq
-          # (python3.withPackages (
-          #   ps: with ps; [
-          #     questionary
-          #     python-lsp-server
-          #   ]
-          # ))
-          basedpyright
-          ruff
           lua-language-server
           nixos-option
           nix-tree
