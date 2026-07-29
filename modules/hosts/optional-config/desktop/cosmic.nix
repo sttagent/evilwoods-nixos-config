788175cac1a27f64c1c4d92aaa243388f5d5af93
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2026-07-29 09:11:46 +0200

    switch to oo7 service

--- modules/hosts/optional-config/desktop/cosmic.nix
+++ modules/hosts/optional-config/desktop/cosmic.nix
@@ -4,6 +4,8 @@
     includes = [ den.aspects.desktop ];
     nixos = {
       services = {
+        gnome.gnome-keyring.enable = false;
+        oo7.enable = true;
         displayManager.cosmic-greeter.enable = true;
         desktopManager.cosmic.enable = true;
       };
